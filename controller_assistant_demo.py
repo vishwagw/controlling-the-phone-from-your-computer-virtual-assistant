86c3a82af413478672cd358b33b06b878a689da4
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-05-01 06:42:39 +0530

    Update controller_assistant_demo.py

--- controller_assistant_demo.py
+++ controller_assistant_demo.py
@@ -1,2 +1,23 @@
 # this is a sample codebase for building the asssitant. 
 # objective: control the smartphone thorugh computer virtual assistant.
+# VERSION 1.7.20
+# 2024:04:30
+
+# adding the phone control function:
+# using gTTS
+# importing libs
+import speech_recognition as sr
+import datetime
+import webbrowser
+import wikipedia
+import psutil
+import os
+import subprocess
+import platform
+from gtts import gTTS
+import pygame
+import tempfile
+import time
+import requests
+import json
+import threading
